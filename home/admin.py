8e0cdca090b352f0dc6c45682cebf75cb508e53e
Filipe <fils.capo@gmail.com>
2024-03-17 01:12:47 -0300

    images and albuns models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Picture, Album
 
-# Register your models here.
+class PicturesAdmin(admin.ModelAdmin):
+    list_display = ('title','cover','album',)
+    search_fields = ['title','cover']
+
+admin.site.register(Picture, PicturesAdmin)
+
+class AlbumnsAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+    search_fields = ['title']
+
+admin.site.register(Album, AlbumnsAdmin)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from core import settings
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)  # Usando TextField para descrições mais longas
+
+    def __str__(self):
+        return self.title
+
+
+class Picture(models.Model):
+    title = models.CharField(max_length=100)
+    file = models.ImageField(upload_to='imagens/')
+    # O 'upload_to' define o subdiretório dentro de MEDIA_ROOT onde as imagens serão salvas
+    album = models.ForeignKey(
+        "Album",
+        on_delete=models.CASCADE,
+        blank = True,
+        null = True,
+    )
+    cover = models.BooleanField(default=False,null=False,blank=False)
+   
+
+    def __str__(self):
+        return f"{self.title} {self.album.id if self.album else ''} {'Capa' if self.cover else 'Não é capa'}"
+    @classmethod
+    def get_media_path(cls):
+        return settings.MEDIA_ROOT
