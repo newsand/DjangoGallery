c5eabad155baa79cea4d55b39af6064a2ae8a192
Filipe <fils.capo@gmail.com>
2024-04-09 17:23:36 -0300

    improve admin
    more fields

--- home/admin.py
+++ home/admin.py
@@ -1,22 +1,28 @@
 from django.contrib import admin
 from .models import Picture, Album, Sitevars
 
+
 class PicturesAdmin(admin.ModelAdmin):
-    list_display = ('title','cover','album',)
-    search_fields = ['title','cover']
-    list_filter = ('album', )
+    list_display = ('title', 'cover', 'album',)
+    search_fields = ['title', 'cover']
+    list_filter = ('album',)
+
 
 admin.site.register(Picture, PicturesAdmin)
 
+
 class AlbumnsAdmin(admin.ModelAdmin):
-    list_display = ('title',)
+    list_display = ('title', 'private')
     search_fields = ['title']
-    list_filter = ('private', )
+    list_filter = ('private',)
+
 
 admin.site.register(Album, AlbumnsAdmin)
 
+
 class SitevarsAdmin(admin.ModelAdmin):
-    list_display = ('name','value','page')
+    list_display = ('name', 'value', 'page')
     search_fields = ['name']
+
 
 admin.site.register(Sitevars, SitevarsAdmin)
