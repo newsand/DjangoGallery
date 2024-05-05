7bc40a0b02353230cb8ead4b8b8542414c0afa1c
Filipe <fils.capo@gmail.com>
2024-05-04 22:30:26 -0300

    removed  all pixl componets views

--- home/views.py
+++ home/views.py
@@ -73,67 +73,6 @@ class UserPasswordChangeView(PasswordChangeView):
   form_class = UserPasswordChangeForm
 
 
-# Components
-def accordion(request):
-  return render(request, 'components/accordions.html')
-
-def alerts(request):
-  return render(request, 'components/alerts.html')
-
-def badges(request):
-  return render(request, 'components/badges.html')
-
-def bootstrap_carousels(request):
-  return render(request, 'components/bootstrap-carousels.html')
-
-def breadcrumbs(request):
-  return render(request, 'components/breadcrumbs.html')
-
-def buttons(request):
-  return render(request, 'components/buttons.html')
-
-def cards(request):
-  return render(request, 'components/cards.html')
-
-def dropdowns(request):
-  return render(request, 'components/dropdowns.html')
-
-def forms(request):
-  return render(request, 'components/forms.html')
-
-def modals(request):
-  return render(request, 'components/modals.html')
-
-def navs(request):
-  return render(request, 'components/navs.html')
-
-def pagination(request):
-  return render(request, 'components/pagination.html')
-
-def popovers(request):
-  return render(request, 'components/popovers.html')
-
-def progress_bars(request):
-  return render(request, 'components/progress-bars.html')
-
-def tables(request):
-  return render(request, 'components/tables.html')
-
-def tabs(request):
-  return render(request, 'components/tabs.html')
-
-def toasts(request):
-  return render(request, 'components/toasts.html')
-
-def tooltips(request):
-  return render(request, 'components/tooltips.html')
-
-def typography(request):
-  return render(request, 'components/typography.html')
-
-
-#
-#
 # def x(request):
 #   # Busca todos os álbuns
 #     vars=Sitevars.objects.filter(page__contains=['index'])
